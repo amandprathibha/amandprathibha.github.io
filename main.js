0c9e9ef3d6d752b4d089a0eeaf0f40581505e446
amandprathibha <prathibha.velaga@gmail.com>
2018-06-10 16:17:24 -0400

    initial version

---
+++ main.js
@@ -0,0 +1,6 @@
+function changeNameColor(id){
+	document.getElementById(id).style.color = 'aqua';
+}
+function changeBackgroundColor(id){
+	document.getElementById(id).style.background = 'lightseagreen';
+}
